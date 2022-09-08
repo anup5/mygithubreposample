8f978fd6915bc732ad923199a80743b11fdfff50
anup4u <8241612+anup5@users.noreply.github.com>
2022-09-08 08:05:04 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,10 @@
+ 
+def add_2_number(num1, num2):
+    sum = num1 + num2
+    print("The sum of {} and {} is {}" .format(num1, num2, sum))
+        
+a = int(input("First number: "))
+b = int(input("Second number: "))
+
+# Calling out user-defined function 
+add_2_number(a, b)
